55be159ace006d8fbb740c265249b80f2cb5dba5
Jorge Gomes <jorge.gomes@vost.pt>
2022-01-08 17:06:45 +0000

    Color Discrete Map
    This map contains the name of all political parties that are running for elections, and respective logo color. 
    The color was picked from the parties website ou wikipedia page.

---
+++ APPS/color_discrete_map.py
@@ -0,0 +1,27 @@
+# Color Discrete Map Legislativas 2022
+
+color_discrete_map={
+                "PCTP/MRPP": "#E00E02",
+				"PPD/PSD": "#FF6400",
+				"CDS/PP":"#0B6AB1",
+				"Ergue-te": "#BF311E",
+				"MAS": "#2B2C2C",
+				"PS": "#E31F26",
+				"LIVRE": "#00CE8C",
+				"ADN": "#204E84",
+				"BE": "#C90635",
+				"PCP - PEV - CDU": "#025298",
+				"R.I.R.": "#00939D",
+				"Volt Portugal": "#502376",
+				"CHEGA": "#202056",
+				"JPP": "#03AB83",
+				"IL": "#00AEEF",
+				"PAN": "#00798E",
+				"MPT": "#66BB69",
+				"PTP": "#00659B",
+				"Nós, Cidadãos!": "#00659B",
+				"ALIANÇA": "#0451CB",
+				"PPD/PSD-CDS-PP - MADEIRA PRIMEIRO": "#F36D24",
+				"PPM" : "#19476C",
+ 				"PPD/PSD - CDS-PP - PPM – AD/Aliança Democrática": "#0031A"
+							}
